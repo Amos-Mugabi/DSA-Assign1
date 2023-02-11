94067a152e1a24bef569bd60da87b31855b827e5
Amos Mugabi <115138015+Amos-Mugabi@users.noreply.github.com>
2023-02-11 19:18:07 +0300

    Add files via upload

---
+++ Num2.py
@@ -0,0 +1,11 @@
+#No. 2
+#Using an if else statement.
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+
+if username == "Martha" and password == "Martha123":
+    print("Hello, Martha! you are welcome")
+    
+else:
+    print("Incorrect username or password. Try again")
+s
